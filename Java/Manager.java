170dfff14ccb5345567a787e0aaaba2c859f6ae6
Varun Ratan <65683442+varun19101995@users.noreply.github.com>
2021-02-10 14:59:14 +0530

    Add files via upload

---
+++ Java/Manager.java
@@ -0,0 +1,17 @@
+package Question1;
+
+public class Manager implements Role{
+
+	@Override
+	public String getRoleName() {
+		// TODO Auto-generated method stub
+		return "Manager";
+	}
+
+	@Override
+	public String getResponsibility() {
+		// TODO Auto-generated method stub
+		return "Management";
+	}
+
+}
